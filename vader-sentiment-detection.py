7114e29e2eaac4d40466caa617d8f92b48e13167
Maksym Tymchenko <64469428+Maksym-Tymchenko@users.noreply.github.com>
2022-01-19 14:56:24 +0000

    Create vader-sentiment-detection.py

---
+++ vader-sentiment-detection.py
@@ -0,0 +1,47 @@
+# import SentimentIntensityAnalyzer class
+# from vaderSentiment.vaderSentiment module.
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+ 
+# function to print sentiments
+# of the sentence.
+def sentiment_scores(sentence):
+ 
+    # Create a SentimentIntensityAnalyzer object.
+    sid_obj = SentimentIntensityAnalyzer()
+ 
+    # polarity_scores method of SentimentIntensityAnalyzer
+    # object gives a sentiment dictionary.
+    # which contains pos, neg, neu, and compound scores.
+    sentiment_dict = sid_obj.polarity_scores(sentence)
+     
+    print("Overall sentiment dictionary is : ", sentiment_dict)
+    print("sentence was rated as ", sentiment_dict['neg']*100, "% Negative")
+    print("sentence was rated as ", sentiment_dict['neu']*100, "% Neutral")
+    print("sentence was rated as ", sentiment_dict['pos']*100, "% Positive")
+ 
+    print("Sentence Overall Rated As", end = " ")
+ 
+    # decide sentiment as positive, negative and neutral
+    if sentiment_dict['compound'] >= 0.05 :
+        print("Positive")
+ 
+    elif sentiment_dict['compound'] <= - 0.05 :
+        print("Negative")
+ 
+    else :
+        print("Neutral")
+ 
+ 
+   
+# Driver code
+if __name__ == "__main__" :
+
+    # Import article from file
+    with open("article.txt") as file:
+        paragraphs  = file.readlines()
+        for paragraph in paragraphs:
+            # analyze sentiment
+            print(paragraph)
+            sentiment_scores(paragraph)
+            # Newline
+            print()
